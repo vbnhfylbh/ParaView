b0732dd1383d420970b66abd4a9fb6f1c786a023
vbnhfylbh <vbnhfylbh@gmail.com>
2016-02-11 21:59:55 +0300

    SanitizeName fixed

--- ParaViewCore/ServerManager/Core/vtkSMCoreUtilities.cxx
+++ ParaViewCore/ServerManager/Core/vtkSMCoreUtilities.cxx
@@ -43,21 +43,21 @@ vtkSMCoreUtilities::~vtkSMCoreUtilities()
 const char* vtkSMCoreUtilities::GetFileNameProperty(vtkSMProxy* proxy)
 {
   if (!proxy)
-    {
+  {
     return NULL;
-    }
+  }
 
   if (proxy->GetHints())
-    {
+  {
     vtkPVXMLElement* filenameHint =
-      proxy->GetHints()->FindNestedElementByName("DefaultFileNameProperty");
+            proxy->GetHints()->FindNestedElementByName("DefaultFileNameProperty");
     if (filenameHint &&
-      filenameHint->GetAttribute("name") &&
-      proxy->GetProperty(filenameHint->GetAttribute("name")))
-      {
+        filenameHint->GetAttribute("name") &&
+        proxy->GetProperty(filenameHint->GetAttribute("name")))
+    {
       return filenameHint->GetAttribute("name");
-      }
     }
+  }
 
   // Find the first property that has a vtkSMFileListDomain. Assume that
   // it is the property used to set the filename.
@@ -65,28 +65,28 @@ const char* vtkSMCoreUtilities::GetFileNameProperty(vtkSMProxy* proxy)
   piter.TakeReference(proxy->NewPropertyIterator());
   piter->Begin();
   while (!piter->IsAtEnd())
-    {
+  {
     vtkSMProperty* prop = piter->GetProperty();
     if (prop && prop->IsA("vtkSMStringVectorProperty"))
-      {
+    {
       vtkSmartPointer<vtkSMDomainIterator> diter;
       diter.TakeReference(prop->NewDomainIterator());
       diter->Begin();
       while(!diter->IsAtEnd())
-        {
+      {
         if (diter->GetDomain()->IsA("vtkSMFileListDomain"))
-          {
-          return piter->GetKey();
-          }
-        diter->Next();
-        }
-      if (!diter->IsAtEnd())
         {
-        break;
+          return piter->GetKey();
         }
+        diter->Next();
       }
-    piter->Next();
+      if (!diter->IsAtEnd())
+      {
+        break;
+      }
     }
+    piter->Next();
+  }
   return NULL;
 }
 
@@ -94,27 +94,27 @@ const char* vtkSMCoreUtilities::GetFileNameProperty(vtkSMProxy* proxy)
 vtkStdString vtkSMCoreUtilities::SanitizeName(const char* name)
 {
   if (!name || name[0] == '\0')
-    {
+  {
     return vtkStdString();
-    }
+  }
 
   std::ostringstream cname;
   for (size_t cc=0; name[cc]; cc++)
+  {
+    if (iswalnum(name[cc]))
     {
-    if (isalnum(name[cc]))
-      {
       cname << name[cc];
-      }
     }
+  }
   // if first character is not an alphabet, add an 'a' to it.
-  if (isalpha(cname.str()[0]))
-    {
+  if (iswalpha(cname.str()[0]))
+  {
     return cname.str();
-    }
+  }
   else
-    {
+  {
     return "a" + cname.str();
-    }
+  }
 }
 
 //----------------------------------------------------------------------------
@@ -122,20 +122,20 @@ bool vtkSMCoreUtilities::AdjustRangeForLog(double range[2])
 {
   assert(range[0] <= range[1]);
   if (range[0] <= 0.0 || range[1] <= 0.0)
-    {
+  {
     // ranges not valid for log-space. Cannot convert.
     if (range[1] <= 0.0)
-      {
+    {
       range[0] = 1.0e-4;
       range[1] = 1.0;
-      }
+    }
     else
-      {
+    {
       range[0] =  range[1] * 0.0001;
       range[0] =  (range[0] < 1.0)? range[0] : 1.0;
-      }
-    return true;
     }
+    return true;
+  }
   return false;
 }
 
@@ -143,27 +143,27 @@ bool vtkSMCoreUtilities::AdjustRangeForLog(double range[2])
 bool vtkSMCoreUtilities::AdjustRange(double range[2])
 {
   if (range[1] < range[0])
-    {
+  {
     // invalid range.
     return false;
-    }
+  }
 
   // the range must be large enough, compared to values order of magnitude
   double rangeOrderOfMagnitude = 1e-11;
   if (rangeOrderOfMagnitude < std::abs(range[0]))
-    {
+  {
     rangeOrderOfMagnitude = std::abs(range[0]);
-    }
+  }
   if (rangeOrderOfMagnitude < std::abs(range[1]))
-    {
+  {
     rangeOrderOfMagnitude = std::abs(range[1]);
-    }
+  }
   double rangeMinLength = rangeOrderOfMagnitude * 1e-5;
   if ((range[1] - range[0]) < rangeMinLength)
-    {
+  {
     range[1] = range[0] + rangeMinLength;
     return true;
-    }
+  }
   return false;
 }
 
